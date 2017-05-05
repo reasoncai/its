02cf1113ad58180e169dccd060b9c0c1b8170c3b
reasoncai <403411876@qq.com>
2017-05-05 09:18:45 +0800

    init

---
+++ src/main/java/com/jiuair/dao/SysLogDao.java
@@ -0,0 +1,14 @@
+package com.jiuair.dao;
+
+import com.jiuair.entity.SysLogEntity;
+
+/**
+ * 系统日志
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-08 10:40:56
+ */
+public interface SysLogDao extends BaseDao<SysLogEntity> {
+	
+}
